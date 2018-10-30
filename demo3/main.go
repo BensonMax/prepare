84df2e1482db06d1ee7c33803be7a2ac3d3d3677
Administrator <65334265@qq.com>
2018-10-30 21:47:30 +0800

    + demo3

--- demo3/main.go
+++ demo3/main.go
@@ -13,6 +13,8 @@ type result struct {
 }
 
 func main() {
+	//执行一个cmd,让它在一个协程里去执行， "sleep 2;echo hello"
+	//1秒的时候，我们杀死cmd
 	var (
 		ctx        context.Context
 		cancelFunc context.CancelFunc
@@ -20,7 +22,7 @@ func main() {
 		resultChan chan *result
 		res        *result
 	)
-
+	//创建一个结果队列
 	resultChan = make(chan *result, 1000)
 
 	ctx, cancelFunc = context.WithCancel(context.TODO())
@@ -32,15 +34,20 @@ func main() {
 		)
 		cmd = exec.CommandContext(ctx, "C:\\cygwin64\\bin\\bash.exe", "-c", "sleep 2;echo hello")
 
+		//执行任务，捕获输出
 		output, err = cmd.CombinedOutput()
+
+		//把任务输出结果传给main协程
 		resultChan <- &result{
 			err,
 			output,
 		}
 	}()
 
+	//睡一秒
 	time.Sleep(1 * time.Second)
 
+	//取消上下文
 	cancelFunc()
 
 	res = <-resultChan
